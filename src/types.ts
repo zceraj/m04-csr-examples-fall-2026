54822221a0896543547b5cb897d7d022e27b5c95
Patrick Zhang <pzhang345@gmail.com>
2026-09-21 12:51:44 -0400

    updated type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,9 @@
 // types.ts - types for the transcript service
 
+
 export type Course = string;
 export type StudentID = number;
 export type StudentName = string;
 export type Student = { studentID: StudentID; studentName: StudentName };
-export type CourseGrade = { course: Course; grade: number };
+export type CourseGrade = { course: Course; grade: number, date: {semester: "Fall" | "Spring" | "Summer", year: number} };
 export type Transcript = { student: Student; grades: CourseGrade[] };
